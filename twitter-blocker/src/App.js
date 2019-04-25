0d3e97685e62164de7eb98c4149b011252330e54
ddot727 <ddot727@gmail.com>
2019-04-25 19:12:21 -0400

    added Twitter Sign Up button

--- twitter-blocker/src/App.js
+++ twitter-blocker/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button href="/login">Sign In Through Twitter</button>
+        <button href="/auth/twitter">Sign In Through Twitter</button>
       </header>
     </div>
   );
