6307c5b84a60a0e60df1486107de6fdafacceaae
Daniel <danielstarling4@gmail.com>
2019-05-01 22:36:28 -0700

    Took out default react landing page.

--- twitter-blocker/src/App.js
+++ twitter-blocker/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Profile from '../src/tweeper/Profile';
 import HelloWorld from '../src/tweeper/HelloWorld';
@@ -10,15 +9,11 @@ const url = process.env.REACT_APP_BACKEND_BASE_URL;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href={`${url}/auth/twitter/login`}>LOGIN</a>
-        Sign In Through Twitter
-      </header>
-      <Route path="/test" component={Profile} />
+      <a href={`${url}/auth/twitter/login`}>
+        <button className="login-button">LOGIN</button>
+      </a>
+      Sign In Through Twitter
+      <Route path="/test" exact component={Profile} />
       <Route path="/hello" component={HelloWorld} />
     </div>
   );
