72623be56031191353f999a3327de7269ed2a786
Erin Meredith <ekmeredith@att.net>
2019-05-07 12:35:55 -0400

    added tabs and basic timeline layout

--- twitter-blocker/src/tweeper/ListDetails.js
+++ twitter-blocker/src/tweeper/ListDetails.js
@@ -16,14 +16,20 @@ import molecules from '../components/molecules';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core'
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { List, ListItem } from '@material-ui/core';
+import { List, ListItem, Tabs, Tab } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { getList, getUser, getListTimeline } from '../actions';
+import { Link } from 'react-router-dom';
 
 library.add(faTimes)
 
 const { Avatar, Icon, Typography, Button } = atoms;
 // const { Tabs, Tab } = molecules;
+
+const TabContainer = styled('div') ({
+  padding: theme.spacing.unit * 4,
+  margin: 'auto'
+})
 
 const Content = styled('div')({
   maxWidth: 1000,
@@ -40,38 +46,72 @@ const Cover = styled('div')({
   backgroundColor: '#ccd6dd',
 });
 
+
 class ListDetails extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      list: []
+      list: [],
+      timeline: [],
+      value: 0
     }
   }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
 
   componentDidMount(){
     this.props.getList(this.props.match.params.id);
     this.setState({note: this.props.list});
+    this.props.getListTimeline(this.props.match.params.id);
+    this.setState({timeline: this.props.timeline})
+  
 }
   
 render() {
+  const { value } = this.state;
   return (
     <React.Fragment>
       <CssBaseline />
       <HeaderTest />
       <Content>
             <Feed>
+              <Tabs onChange={this.handleChange}>
+                <Tab label='Members'/>
+                <Tab label="Timeline"/>
+                </Tabs>
+                {value === 0 &&
+              <TabContainer>
               <List>
                 {this.props.list.map(i => {
                   return (
                   <ListItem>
-                    <Avatar />
-                    <Typography>{i.name}</Typography>
+                    <Avatar src={i.profile_img}/>
+                    <Link to={`/test/${this.props.twitter_id}`}><Typography>{i.name}</Typography></Link>
                     <Typography>{i.screen_name}</Typography>
                     <Typography>{i.description}</Typography>
                     <FontAwesomeIcon icon="times" />
                   </ListItem>)
                 })}
               </List>
+              </TabContainer>
+              }
+              {value === 1 &&
+              <TabContainer>
+                <List>
+                  {this.props.timeline.map(i => {
+                    return (
+                      <div>
+                        <Typography>{i.user.name}</Typography>
+                      <Typography>{i.entities.text}</Typography>
+                      <Typography>{i.entities.hashtags.text}</Typography>
+                      </div>
+                    )
+                  })}
+                  </List>
+                </TabContainer>
+              }
               <Divider />
             </Feed>
         <TweetFloat />
@@ -83,7 +123,8 @@ render() {
 
 const mapStateToProps = state => {
   return {
-    list: state.listsReducer.list
+    list: state.listsReducer.list,
+    timeline: state.listsReducer.listTimeline
   }
 }
 
