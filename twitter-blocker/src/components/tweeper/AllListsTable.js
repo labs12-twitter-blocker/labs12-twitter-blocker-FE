43b22574e39f91c0340298610875146fa4dc6ccb
ddot727 <ddot727@gmail.com>
2019-05-09 22:25:27 -0400

    switched out create list placeholder button with actual component

--- twitter-blocker/src/components/tweeper/AllListsTable.js
+++ twitter-blocker/src/components/tweeper/AllListsTable.js
@@ -9,7 +9,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import { getUserList } from '../../actions/index'
+import { getUserList } from '../../actions/index';
+import CreateList from './CreateList';
 
 const styles = theme => ({
   root: {
@@ -31,84 +32,82 @@ function createData(list_name, description, member_count, subscriber_count, list
 
 
 class AllListsTable extends Component {
-    state={
-        rows: [],
-        listRan: false,
+  state = {
+    rows: [],
+    listRan: false,
+  }
+
+
+  componentDidMount() {
+    // this.props.getLists()
+    this.props.getUserList(localStorage.getItem("twitter_user_id"))
+  };
+
+  componentDidUpdate() {
+    if (this.props.lists.length > 0 && this.state.listRan === false) {
+      this.getListRowBuilder(this.props.lists);
     }
+  }
 
-    
-    componentDidMount() {
-        // this.props.getLists()
-        this.props.getUserList(localStorage.getItem("twitter_user_id"))
-    };
+  getListRowBuilder = (list) => {
+    let newRow = [];
+    // console.log('here')
+    list.map(list => {
+      // console.log("list", list);
+      newRow.push(createData(list.list_name, list.description,
+        list.member_count, list.subscriber_count,
+        list.list_upvotes, list.list_downvotes))
+    })
+    // console.log(newRow);
+    this.setState({ listRan: true })
+    this.setState({ rows: newRow });
+  };
 
-    componentDidUpdate() {
-        if (this.props.lists.length > 0 && this.state.listRan === false) {
-            this.getListRowBuilder(this.props.lists);
-        }
+
+
+  render() {
+
+    if (this.props.lists === null || this.props.lists.length === 0) {
+      return (<div>
+        <h3>Create Your First List!</h3>
+        <CreateList />
+      </div>)
+    } else {
+      const { classes } = this.props;
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>List Name</TableCell>
+                <TableCell align="center">List Description</TableCell>
+                <TableCell align="center">List Members</TableCell>
+                <TableCell align="center">List Subsribers</TableCell>
+                <TableCell align="center">List Up Votes</TableCell>
+                <TableCell align="center">List Down Votes</TableCell>
+                {/* <TableCell align="center">Protein (g)</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {console.log("this.state.rows", this.state.rows)} */}
+              {this.state.rows.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.list_name}
+                  </TableCell>
+                  <TableCell align="center">{row.description}</TableCell>
+                  <TableCell align="center">{row.member_count}</TableCell>
+                  <TableCell align="center">{row.subscriber_count}</TableCell>
+                  <TableCell align="center">{row.list_upvotes}</TableCell>
+                  <TableCell align="center">{row.list_downvotes}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
     }
-
-    getListRowBuilder = (list) => {
-        let newRow = [];
-        // console.log('here')
-        list.map(list => {
-            // console.log("list", list);
-            newRow.push(createData(list.list_name, list.description, 
-                list.member_count, list.subscriber_count, 
-                list.list_upvotes, list.list_downvotes))
-        })
-        // console.log(newRow);
-        this.setState({listRan: true})
-        this.setState({rows: newRow});
-    };
-
-
-
-    render() {
-        
-        if (this.props.lists === null || this.props.lists.length === 0) {
-            return (<div>
-              <h3>Create Your First List!</h3>
-              <Button medium color="primary" variant="contained">
-                Create New List
-              </Button>
-              </div>)
-        } else {
-        const { classes } = this.props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>List Name</TableCell>
-            <TableCell align="center">List Description</TableCell>
-            <TableCell align="center">List Members</TableCell>
-            <TableCell align="center">List Subsribers</TableCell>
-            <TableCell align="center">List Up Votes</TableCell>
-            <TableCell align="center">List Down Votes</TableCell>
-            {/* <TableCell align="center">Protein (g)</TableCell> */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-            {/* {console.log("this.state.rows", this.state.rows)} */}
-          {this.state.rows.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.list_name}
-              </TableCell>
-              <TableCell align="center">{row.description}</TableCell>
-              <TableCell align="center">{row.member_count}</TableCell>
-              <TableCell align="center">{row.subscriber_count}</TableCell>
-              <TableCell align="center">{row.list_upvotes}</TableCell>
-              <TableCell align="center">{row.list_downvotes}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
- }
-}
+  }
 }
 
 AllListsTable.propTypes = {
@@ -118,9 +117,9 @@ AllListsTable.propTypes = {
 // export default withStyles(styles)(AllListsTable);
 
 const mapStateToProps = state => ({
-    lists: state.listsReducer.userLists
-  });
-  
+  lists: state.listsReducer.userLists
+});
+
 
 const styledComponent = withStyles(styles)(AllListsTable);
 
