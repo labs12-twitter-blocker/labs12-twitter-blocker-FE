31aa9765239c3178a1012ac5505941e3bd15de9e
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-05-15 11:29:20 -0700

    Merge Conflict

--- twitter-blocker/src/tests/HeaderTest.js
+++ twitter-blocker/src/tests/HeaderTest.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import Hidden from '@material-ui/core/Hidden';
+// import Hidden from '@material-ui/core/Hidden';
 import CreateList from '../components/tweeper/CreateList';
-import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import classNames from 'classnames';
+// import { withStyles } from '@material-ui/core/styles';
+// import Fab from '@material-ui/core/Fab';
+// import classNames from 'classnames';
 import TextField from '@material-ui/core/TextField';
 // import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -14,7 +14,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faHome, faSearch, faBell, faEnvelope, faList, faCog } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faHome, faSearch, faList, faCog, faTrophy } from '@fortawesome/free-solid-svg-icons';
 import atoms from '../components/atoms';
 import molecules from '../components/molecules';
 import { searchLists } from '../actions';
@@ -63,76 +63,86 @@ class HeaderTest extends React.Component {
   render() {
     return (
       <div>
-        <AppBar position="sticky" elevation={1}>
-          <Toolbar>
-            <Grid container alignItems="center" spacing={16}>
-              <Grid item xs={12} sm={4}>
-                <Tabs value={0} variant="fullWidth">
-                  <Tab
-                    onlyIcon
-                    icon={
-                      <Badge dotted badgeContent="">
-                        <Link to="/profile">
-                          <FontAwesomeIcon icon={faHome} size="2x" color='#38A1F3' />
-                        </Link>
-                      </Badge>
-                    }
-                  />
-                  <Tab onClick={this.handleClickOpen}
-                    onlyIcon
-                    icon={
-                      <FontAwesomeIcon icon={faPlus} size="2x" color='#38A1F3' />
-                    }
-                  />
-                  <Dialog
-                    open={this.state.open}
-                    onClose={this.handleClose}
-                    aria-labelledby="form-dialog-title"
-                  >
-                    <DialogTitle id="form-dialog-title">Tweet in Peace ✌️</DialogTitle>
-                    <DialogContent>
-                      <DialogContentText>
-                        Compose Your Tweet Below
-            </DialogContentText>
-                      <TextField style={tweetBox}
-                        autoFocus
-                        margin="normal"
-                        id="tweet"
-                        label="Tweet"
-                        variant="outlined"
-                        //   value={this.state.name}
-                        inputProps={{ maxLength: 280 }}
-                        fullWidth
-                      />
-                    </DialogContent>
-                    <DialogActions>
-                      <Button onClick={this.handleClose} color="primary">
-                        Cancel
-            </Button>
-                      <Button onClick={this.handleClose} color="primary">
-                        Post Tweet
-            </Button>
-                    </DialogActions>
-                  </Dialog>
-                  <Tab
-                    onlyIcon
-                    icon={
-                      <Badge >
-                        <Link to="/explorer">
-                          <FontAwesomeIcon icon={faList} size="2x" color='#38A1F3' />
-                        </Link>
-                      </Badge>
-                    }
-                  />
-                  <Tab onlyIcon icon={
-                    <Link to="/settings">
-                      <FontAwesomeIcon icon={faCog} size="2x" color='#38A1F3' />
-                    </Link>} />
-                  {/* <Tab onlyIcon icon={<FontAwesomeIcon icon={faEnvelope} size="2x" color='#38A1F3'/>} /> */}
-                </Tabs>
-              </Grid>
-
-              <Grid item sm xs={12}>
+      <AppBar position="sticky" elevation={1}>
+        <Toolbar>
+          <Grid container alignItems="center" spacing={16}>
+            <Grid item xs={12} sm={4}>
+              <Tabs value={0} variant="fullWidth">
+                <Tab
+                  onlyIcon
+                  icon={
+                    <Badge dotted badgeContent="">
+                      <Link to="/profile">
+                        <FontAwesomeIcon icon={faHome} size="2x" color='#38A1F3'/>
+                      </Link>
+                    </Badge>
+                  }
+                />
+                <Tab onClick={this.handleClickOpen}
+                        onlyIcon 
+                        icon={
+                            <FontAwesomeIcon  icon={faPlus} size="2x" color='#38A1F3' />
+                        }
+                        />
+                <Dialog
+                  open={this.state.open}
+                  onClose={this.handleClose}
+                  aria-labelledby="form-dialog-title"
+                >
+                  <DialogTitle id="form-dialog-title">Tweet in Peace ✌️</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      Compose Your Tweet Below
+                    </DialogContentText>
+                    <TextField style={tweetBox}
+                      autoFocus
+                      margin="normal"
+                      id="tweet"
+                      label="Tweet"
+                      variant="outlined"
+                    //   value={this.state.name}
+                      inputProps={{ maxLength: 280 }}
+                      fullWidth
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                      Cancel
+                    </Button>
+                    <Button onClick={this.handleClose} color="primary">
+                      Post Tweet
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+                <Tab
+                  onlyIcon
+                  icon={
+                    <Badge >
+                      <Link to="/explorer">
+                        <FontAwesomeIcon icon={faList} size="2x" color='#38A1F3'/>
+                      </Link>
+                    </Badge>
+                  }
+                />
+                <Tab
+                  onlyIcon
+                  icon={
+                    <Badge >
+                      <Link to="/leaderboard">
+                        <FontAwesomeIcon icon={faTrophy} size="2x" color='#38A1F3'/>
+                      </Link>
+                    </Badge>
+                  }
+                />
+                <Tab onlyIcon icon={
+                      <Link to="/settings">
+                        <FontAwesomeIcon icon={faCog} size="2x" color='#38A1F3' />
+                      </Link>}/>
+                {/* <Tab onlyIcon icon={<FontAwesomeIcon icon={faEnvelope} size="2x" color='#38A1F3'/>} /> */}
+              </Tabs>
+            </Grid>
+            
+            <Grid item sm xs={12}>
                 <TextField
                   fullWidth
                   placeholder="Find Lists"
@@ -140,28 +150,29 @@ class HeaderTest extends React.Component {
                     disableUnderline: true,
                     startAdornment: (
                       <InputAdornment position="start" style={searchIcon}>
-                        <FontAwesomeIcon icon={faSearch} color='#38A1F3' />
+                        <FontAwesomeIcon icon={faSearch} color='#38A1F3'/>
                       </InputAdornment>
                     ),
                   }}
                   onChange={this.searchLists}
                 />
               </Grid>
-
-              <Grid item xs={6} sm="auto" >
-                <ListItem>
-                  <Avatar src="./assets/austen.png" style={avatarStyle} />
-                  {/* <ListItemText primary="austen" /> */}
-                  <React.Fragment>
-                    <Button medium color="primary" variant="contained" href="/create">
-                      Create New List
-                  </Button>
-                  </React.Fragment>
-                </ListItem>
-              </Grid>
+          
+            <Grid item xs={6} sm="auto" >
+              <ListItem>
+                <Avatar src="./assets/austen.png" style={avatarStyle} />
+                {/* <ListItemText primary="austen" /> */}
+                <React.Fragment>
+                  {/* <Button medium color="primary" variant="contained">
+                        Create New List
+                      </Button> */}
+                  <CreateList />
+                </React.Fragment>
+              </ListItem>
             </Grid>
-          </Toolbar>
-        </AppBar>
+          </Grid>
+        </Toolbar>
+      </AppBar>
       </div>
     );
   }
