b496ae46df5265935ce9e56ba358f031edbbd3c9
Daniel <danielstarling4@gmail.com>
2019-05-22 17:32:09 -0700

    master

--- twitter-blocker/src/tweeper/ListDetails.js
+++ twitter-blocker/src/tweeper/ListDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider/Divider';
@@ -106,10 +106,10 @@ class ListDetails extends React.Component {
   };
 
   subscribe = () => {
-      this.props.subscribeToList(this.props.list.twitter_list_id, this.state.twitter_user_id);
-      this.setState({isSubscribed: true})
-      console.log("this.props.list.twitter_list_id", this.props.list.twitter_list_id)
-      console.log("this.state.twitter_user_id", this.state.twitter_user_id)
+    this.props.subscribeToList(this.props.list.twitter_list_id, this.state.twitter_user_id);
+    this.setState({ isSubscribed: true })
+    console.log("this.props.list.twitter_list_id", this.props.list.twitter_list_id)
+    console.log("this.state.twitter_user_id", this.state.twitter_user_id)
   }
 
   unsubscribe = () => {
@@ -146,21 +146,21 @@ class ListDetails extends React.Component {
     this.props.getListTimeline(this.props.match.params.twitter_list_id, decoded.id);
     this.props.getListSubscribers(this.props.match.params.twitter_list_id);
     this.props.listSubscribers.map(user => {
-      if(user.twitter_user_id === decoded.id) {
-        this.setState({isSubscribed: true})
+      if (user.twitter_user_id === decoded.id) {
+        this.setState({ isSubscribed: true })
       }
     })
 
-}
+  }
 
-render() {
-  const { value } = this.state;
-  const { isSubscribed } = this.state;
+  render() {
+    const { value } = this.state;
+    const { isSubscribed } = this.state;
 
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <BackButton />
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <BackButton />
         <Content>
           <Feed>
             <DetailsHeader>
@@ -172,50 +172,52 @@ render() {
                 </Grid>
                 <Grid item>
                   {isSubscribed === false &&
-                    <SubscribeButton color="primary" variant="outlined" color="primary" style={{color:"#1da1f2", border:"2px solid #1da1f2"}} onClick={this.subscribe}>Subscribe</SubscribeButton>
+                    <SubscribeButton color="primary" variant="outlined" color="primary" style={{ color: "#1da1f2", border: "2px solid #1da1f2" }} onClick={this.subscribe}>Subscribe</SubscribeButton>
                   }
                   {isSubscribed === true &&
                     <SubscribeButton color="primary" variant="contained" onClick={this.unsubscribe}>Unsubscribe</SubscribeButton>
                   }
                 </Grid>
-              </DetailsHeader>
+              </Grid>
+            </DetailsHeader>
 
-              <Tabs
-                value={value}
-                onChange={this.handleChange}
-                variant='fullWidth' >
-                <Tab label='Members' />
-                <Tab label='Timeline'/>
-                </Tabs>
+            <Tabs
+              value={value}
+              onChange={this.handleChange}
+              variant='fullWidth' >
+              <Tab label='Members' />
+              <Tab label='Timeline' />
+            </Tabs>
 
 
-                {value === 0 &&
+            {value === 0 &&
               <TabContainer>
 
-              <Grid container spacing={8} direction="column" alignItems="center" justify="center" >
-                <Grid item xs={10} sm={8} md={6} style={{width:"100%"}}>
-                {this.props.listMembers.map(i => {
-                  return (
-                    <List key={i.screen_name}>
-                    <Card >
-                      <ListItem>
-                      <CardContent style={{width:'100%'}}>
-                        <TopLine>
-                          <ProfileNameImg>
-                            <Avatar src={i.profile_img} style={{marginRight: '5px'}}/>
-                            <Link to={`/profile/${i.twitter_user_id}`} style={{textDecoration:'none'}}><ProfileName >{i.name}</ProfileName></Link>
-                            </ProfileNameImg>
-                        <Typography>@{i.screen_name}</Typography>
-                        </TopLine>
-                        <Typography>{i.description}</Typography>
-                        {/* {localStorage.getItem("twitter_user_id") === this.props.list.twitter_id ? <FontAwesomeIcon icon="times" onClick={this.removeFromList(i)}/> : null} */}
-                        </CardContent>
-                        </ListItem>
-                    </Card>
-                    </List>
-                )})}
+                <Grid container spacing={8} direction="column" alignItems="center" justify="center" >
+                  <Grid item xs={10} sm={8} md={6} style={{ width: "100%" }}>
+                    {this.props.listMembers.map(i => {
+                      return (
+                        <List key={i.screen_name}>
+                          <Card >
+                            <ListItem>
+                              <CardContent style={{ width: '100%' }}>
+                                <TopLine>
+                                  <ProfileNameImg>
+                                    <Avatar src={i.profile_img} style={{ marginRight: '5px' }} />
+                                    <Link to={`/profile/${i.twitter_user_id}`} style={{ textDecoration: 'none' }}><ProfileName >{i.name}</ProfileName></Link>
+                                  </ProfileNameImg>
+                                  <Typography>@{i.screen_name}</Typography>
+                                </TopLine>
+                                <Typography>{i.description}</Typography>
+                                {/* {localStorage.getItem("twitter_user_id") === this.props.list.twitter_id ? <FontAwesomeIcon icon="times" onClick={this.removeFromList(i)}/> : null} */}
+                              </CardContent>
+                            </ListItem>
+                          </Card>
+                        </List>
+                      )
+                    })}
+                  </Grid>
                 </Grid>
-              </Grid>
 
 
               </TabContainer>
@@ -226,29 +228,29 @@ render() {
 
                   {this.props.timeline.map(i => {
                     return (
-                      <Grid item xs={10} sm={8} md={6} style={{width:"100%"}}>
-                    <List>
-                      <Card>
-                      <CardContent style={{width:'100%'}}>
-                      <Tweet name={i.user.name} profileImg={i.user.profile_image_url} text={i.text} date={i.created_at}/>
+                      <Grid item xs={10} sm={8} md={6} style={{ width: "100%" }}>
+                        <List>
+                          <Card>
+                            <CardContent style={{ width: '100%' }}>
+                              <Tweet name={i.user.name} profileImg={i.user.profile_image_url} text={i.text} date={i.created_at} />
 
-                    </CardContent>
-                    </Card>
-                    </List>
-                  </Grid>
+                            </CardContent>
+                          </Card>
+                        </List>
+                      </Grid>
                     )
                   })}
-                  </Grid>
+                </Grid>
 
-                </TabContainer>
-              }
-              <Divider />
-            </Feed>
-        {/* <TweetFloat /> */}
-      </Content>
-    </React.Fragment>
-  );
-}
+              </TabContainer>
+            }
+            <Divider />
+          </Feed>
+          {/* <TweetFloat /> */}
+        </Content>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => {
